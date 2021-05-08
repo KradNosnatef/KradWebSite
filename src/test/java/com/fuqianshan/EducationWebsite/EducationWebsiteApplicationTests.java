e44f8d31e7edc083a0ef88c7990471b921610521
fuqianshan <fuqianshan@foxmail.com>
2021-05-08 13:56:42 +0800

    添加项目文件。

---
+++ src/test/java/com/fuqianshan/EducationWebsite/EducationWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuqianshan.EducationWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
